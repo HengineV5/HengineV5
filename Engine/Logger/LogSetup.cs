1c2e80145c4ecf8f29a6d6d4feefadef0fab9ac2
Henrik Strocka <henstr@hotmail.com>
2025-07-08 18:52:33 +0200

    Added logsetup

---
+++ Engine/Logger/LogSetup.cs
@@ -0,0 +1,16 @@
+﻿using ImageLib;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+	public static class LogSetup
+	{
+		public static void LoggerSetup(ILoggerFactory factory)
+		{
+			ImageLibLog.SetLoggerFactory(factory);
+		}
+	}
+}
